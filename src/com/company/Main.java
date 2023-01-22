6b4bc21726e14d02478cbfee2ffd646cce542b0e
Jon Tokad <jontokad@gmail.com>
2023-01-22 02:11:04 -0500

    added gui for main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,33 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Main extends JComponent implements Runnable{
+
+    private Data data;
+
+
     JButton fordButton;
     JButton hillyButton;
     JButton wileyButton;
     JButton earhartButton;
     JButton windsorButton;
 
-    public Main() {
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
+    JLabel swipesLabel;
+    JLabel openLabel;
+    JLabel waitLabel;
+    JLabel swipesLabel2;
+    JLabel openLabel2;
+    JLabel waitLabel2;
 
-            }
-        });
 
-    }
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Main());
@@ -28,15 +35,19 @@ public class Main extends JComponent implements Runnable{
         frame.setTitle("PurdueDiningPlus");
 
     }
+
     public void run()
     {
         JFrame frame = new JFrame("Purdue Dining Plus");
+
         Container content = frame.getContentPane();
         content.setLayout(new BorderLayout());
 
         Main main = new Main();
 
-        JPanel panel = new JPanel();
+        JPanel panelButtons = new JPanel();
+        JPanel panelLabels = new JPanel();
+        JPanel panelLabels2 = new JPanel();
 
         content.add(main, BorderLayout.CENTER);
         content.add(main);
@@ -47,23 +58,81 @@ public class Main extends JComponent implements Runnable{
         frame.setVisible(true);
 
         fordButton = new JButton("Ford");
-        panel.add(fordButton);
-        content.add(panel, BorderLayout.NORTH);
+        panelButtons.add(fordButton);
+        content.add(panelButtons, BorderLayout.NORTH);
 
         hillyButton = new JButton("Hillenbrand");
-        panel.add(hillyButton);
-        content.add(panel, BorderLayout.NORTH);
+        panelButtons.add(hillyButton);
+        content.add(panelButtons, BorderLayout.NORTH);
 
         wileyButton = new JButton("Willey");
-        panel.add(wileyButton);
-        content.add(panel, BorderLayout.NORTH);
+        panelButtons.add(wileyButton);
+        content.add(panelButtons, BorderLayout.NORTH);
 
         earhartButton = new JButton("Earhart");
-        panel.add(earhartButton);
-        content.add(panel, BorderLayout.NORTH);
+        panelButtons.add(earhartButton);
+        content.add(panelButtons, BorderLayout.NORTH);
 
         windsorButton = new JButton("Windsor");
-        panel.add(windsorButton);
+        panelButtons.add(windsorButton);
+        content.add(panelButtons, BorderLayout.NORTH);
+
+        swipesLabel = new JLabel("Swipes:");
+        panelLabels.add(swipesLabel);
+        content.add(panelLabels, BorderLayout.CENTER);
+
+        openLabel = new JLabel("Open:");
+        panelLabels.add(openLabel);
+        content.add(panelLabels, BorderLayout.CENTER);
+
+        waitLabel = new JLabel("Wait:");
+        panelLabels.add(waitLabel);
+        content.add(panelLabels, BorderLayout.CENTER);
+
+        swipesLabel2 = new JLabel(String.valueOf(data.swipes));
+        panelLabels2.add(swipesLabel2);
+        content.add(panelLabels2, BorderLayout.CENTER);
+
+        openLabel2 = new JLabel(String.valueOf(data.isOpen));
+        panelLabels2.add(openLabel2);
+        content.add(panelLabels2, BorderLayout.CENTER);
+
+        waitLabel2 = new JLabel(String.valueOf(data.wait));
+        panelLabels2.add(waitLabel2);
+        content.add(panelLabels2, BorderLayout.CENTER);
+
+        /* connect to socket       */
+        /* if failure exit program */
+
+        Socket socket;
+
+        try {
+            socket = new Socket("localhost", 4242);
+        } catch (UnknownHostException e) {
+            JOptionPane.showMessageDialog(null, "This host does not exist", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "I/O Error in connection.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        /* get data from server */
+
+        try {
+            ObjectInputStream clientReader = new ObjectInputStream(socket.getInputStream());
+            data = (Data) clientReader.readObject();
+        } catch (IOException e) {
+            //end program when there is an I/O exception
+            JOptionPane.showMessageDialog(null, "I/O Error.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Class Not Found Error.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
 
         fordButton.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent e ){
